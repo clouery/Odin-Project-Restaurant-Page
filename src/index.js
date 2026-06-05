932a55aedbc94c5d7a0e1a6a8f0f77a087fd111a
Hor Xiang Zhi <horxiangzhi@gmail.com>
2026-06-06 00:56:16 +0800

    Prep for menu rendering

--- src/index.js
+++ src/index.js
@@ -1,7 +1,29 @@
 // This is the default page (home page) to be generated
 import "./styles.css";
 
+import homeBg from "./img/homebg.jpg";
 
+// 
+const headerHomeBtn = document.querySelector(".toHome");
+headerHomeBtn.addEventListener('click', (event) => {
+    window.location.reload();
+})
 
+const homeBtn = document.querySelector(".home-btn");
+homeBtn.addEventListener('click', (event) => {
+    console.log("home btn clicked. Rendering home now!");
+    renderHome();
+});
+
+function renderHome() {
+
+    document.body.style.backgroundImage = `url(${homeBg})`;
+    document.body.style.backgroundSize = 'cover';
+    document.querySelector(".home-btn").style.visibility = 'hidden';
+    document.querySelector(".menu-btn").style.visibility = 'hidden';
+
+    const para = document.createElement('p');
+    para.textContent = "There is abosolutely nothing here for now"
+}
 
 
