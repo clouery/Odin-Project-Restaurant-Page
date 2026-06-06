b26bc452944d537e26a72294fd874b7b264042ff
Hor Xiang Zhi <horxiangzhi@gmail.com>
2026-06-07 02:49:14 +0800

    completed restaurant page

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@
 import "./styles.css";
 import { renderMenu } from "./menu.js";
 
-
 // THIS IS HOME RENDER
 // TODO: Change to a different Bg img
 import homeBg from "./img/swords.jpg";
@@ -12,30 +11,63 @@ headerHomeBtn.addEventListener('click', (event) => {
   window.location.reload();
 })
 
+
+
 const homeBtn = document.querySelector(".home-btn");
 homeBtn.addEventListener('click', (event) => {
+  event.preventDefault();
+
   console.log("home btn clicked. Rendering home now!");
-  renderHome();
+  renderCredits();
 });
 
-function renderHome() {
+function clearGenerated() {
+  const contents = document.getElementById('contents');
+  Array.from(contents.children).forEach(child => {
+    if (!child.matches('nav')) child.remove();
+  });
+}
 
+function renderCredits() {
+  clearGenerated();
   document.body.style.backgroundImage = `url(${homeBg})`;
   document.body.style.backgroundSize = 'cover';
   document.querySelector(".home-btn").style.visibility = 'hidden';
   document.querySelector(".menu-btn").style.visibility = 'hidden';
 
+
+  const creditBox = document.createElement('div');
+  creditBox.classList.add('credit-box');
+
+
   const para = document.createElement('p');
-  para.textContent = "There is abosolutely nothing here for now"
+  para.classList.add("creditsPara");
+  para.textContent = "ME and images from the internet";
+
+  creditBox.appendChild(para);
+
+  const contents = document.getElementById('contents');
+  contents.appendChild(creditBox);
+
 }
 
 
 // THIS IS MENU RENDER
 
-const curImg = 1;
+let curImg = 1;
 
 const menuBtn = document.querySelector('.menu-btn');
-menuBtn.addEventListener('click', renderMenu);
+menuBtn.addEventListener('click', (event) => {
+  event.preventDefault();
+
+  renderMenu(curImg++);
+
+  // add a img counter
+  if (curImg === 4) {
+    curImg = 1;
+  };
+
+});
 
 
 
--- src/menu.js
+++ src/menu.js
@@ -1,22 +1,70 @@
 import menuBg from "./img/homebg.jpg";
 import css from "./styles.css";
 
-export function renderMenu() {
+// IMPORT MENU IMGS
+import menu1 from "./img/page1.png";
+import menu2 from "./img/page2.png";
+import menu3 from "./img/page3.png";
+  
+
+const contents = document.getElementById('contents');
+export function renderMenu(curImg) {
   document.body.style.backgroundImage = `url(${menuBg})`;
   document.body.style.backgroundSize = "cover";
 
   document.querySelector(".home-btn").style.visibility = "hidden";
   document.querySelector(".menu-btn").style.visibility = "hidden";
 
-  const nextItemBtn = document.createElement('button');
-  nextItemBtn.classList.add("nextItemBtn");
-  nextItemBtn.textContent = "Next";
+  let nextItemBtn = document.querySelector(".nextItemBtn");
 
+  if (!nextItemBtn) {
+    nextItemBtn = document.createElement('button');
+    nextItemBtn.classList.add("nextItemBtn");
+    nextItemBtn.textContent = "Next";
+    contents.appendChild(nextItemBtn);
+  }
 
-  const contents = document.getElementById('contents');
-  contents.appendChild(nextItemBtn);
+  // ADD MENU 1 here; curImgNum ++?
+  renderImg(curImg);
+
+  nextItemBtn.onclick = (event) => {
+    event.preventDefault();
+
+    curImg = (curImg % 3) + 1;
+    renderImg(curImg);
+  };
 }
 
 function renderImg(curImgNum) {
-  
+  let imgNum = curImgNum;
+  let pageRender;
+  if(imgNum === 1){
+    pageRender = menu1;
+  } else if(imgNum === 2){
+    pageRender = menu2;
+  } else if (imgNum === 3) {
+    pageRender = menu3;
+  } else {
+    imgNum= 1;
+    pageRender = menu1;
+  }
+
+  const existingImg = document.querySelector(".menu-img");
+  if (existingImg) {
+    existingImg.src = pageRender;
+    return;
+  }
+
+  // create flex box
+  const page = document.createElement('div');
+  const img = document.createElement('img');
+  page.classList.add("menu-box");
+  img.classList.add("menu-img");
+  img.src = pageRender;
+  page.appendChild(img);
+
+  // add to the contents
+  contents.appendChild(page);
+
+
 }
